8ba7ce049c1af9be152a0c10058d0e0cf7d228ce
admin-kazuto <147340489+admin-kazuto@users.noreply.github.com>
2024-08-23 00:31:05 +0700

    sửa lại toàn bộ

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import productRouter from "./productRouter";
-
-export default function routes(app){
-    app.get("/",(req,res)=> {
-        res.send({message:"sever đang hoạt động"});
-    });
-    app.use("/products",productRouter)
-    app.use("/categor",categoryRouter) // chưa thêm nên chưa có
-}
