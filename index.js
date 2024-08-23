7750365bbe319cd82f2911474ba0ef8fca33c29d
admin-kazuto <147340489+admin-kazuto@users.noreply.github.com>
2024-08-23 10:42:41 +0700

    update check request

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const morgan = require("morgan")
 const app = express();
 const port = 3000;
+
+app.use(morgan('combined')) // dùng để check request
 app.get("/", function (req,res) {
   return res.send("hello world");
 });
