0e5faf1f80b3f8f74648bd57cc14b3c71bbd1a63
admin-kazuto <147340489+admin-kazuto@users.noreply.github.com>
2024-08-24 18:16:08 +0700

    update đường dẫn đến file ảnh

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 // import { engine } from "express-handlebars";
-const path = require("path")
+const path = require("path");
 const { engine } = require("express-handlebars");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "resources/public")));
 
 app.use(morgan("combined")); // dùng để check request
 
-app.engine("hbs", engine({
-    extname : '.hbs'
-})); //templace engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+); //template engine
 app.set("view engine", "hbs");
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set("views", path.join(__dirname, "resources/views"));
 
-app.enable('view cache') //kiểm soát cache
+app.enable("view cache"); //kiểm soát cache
 
 // console.log("PATH", path.join(__dirname,''))
-app.get("/", (req, res) => res.render('home'));
-app.get("/news", (req, res) => res.render('news'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/news", (req, res) => res.render("news"));
 app.listen(port, () => console.log(`http://localhost:${port}`));
-
