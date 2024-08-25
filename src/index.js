330786948c0c7359c215cd32f1da789fd8f237d8
admin-kazuto <147340489+admin-kazuto@users.noreply.github.com>
2024-08-25 22:16:55 +0700

    update render scss sang css và thêm extensions scss vào file nodemon.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import { engine } from "express-handlebars";
 const path = require("path");
+const sass = require('sass');
 const { engine } = require("express-handlebars");
 const express = require("express");
 const morgan = require("morgan");
@@ -7,6 +8,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "resources/public")));
+
+//cách này cũng được nhưng mà vẫn lỗi chưa sửa được
+// const scssFile = path.join(__dirname, "resources/scss/app.scss") //đường dẫn tới file chứa scss
+// const result = sass.compile(scssFile);  // biên dịch file scss
+// fs.writeFileSync(path.join(__dirname, 'resouces/public/css/stylel.css'), result.css); // ghi ra file scss khi biên dịch
 
 app.use(morgan("combined")); // dùng để check request
 
